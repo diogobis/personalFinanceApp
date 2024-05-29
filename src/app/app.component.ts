4c65c364bd389e51ced17150f17a4b7b69226096
Diogo Alves <diogobis23@gmail.com>
2024-05-29 03:59:52 -0300

    Angular config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+import {
+  NgxEchartsDirective,
+  NgxEchartsModule,
+  provideEcharts,
+} from 'ngx-echarts';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidemenuComponent, NgxEchartsModule, DecimalPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [provideEcharts(), DecimalPipe],
 })
 export class AppComponent {
   title = 'personalFinanceFrotend';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TransactionFormComponent } from './pages/transaction-form/transaction-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'transaction', component: TransactionFormComponent},
+  { path: 'home', component: HomeComponent },
+];

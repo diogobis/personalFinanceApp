019c50cde4936cc4cc925f63f9b0c295d8f3ce07
Diogo Alves <diogobis23@gmail.com>
2024-05-29 03:58:56 -0300

    Side-menu template

---
+++ src/app/components/sidemenu/sidemenu.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.css',
+})
+export class SidemenuComponent {
+  public routes: any = [
+    {
+      label: 'Main',
+      links: [
+        { path: 'home', label: 'Home' },
+        { path: 'transaction', label: 'Transactions' },
+      ],
+    },
+    {
+      label: 'Settings',
+      links: [
+        { path: 'types', label: 'Transaction Types' },
+        { path: 'accounts', label: 'Accounts' },
+      ],
+    },
+    {
+      label: 'Logout',
+      links: [{ path: 'login', label: 'Sign out' }],
+    },
+  ];
+}
